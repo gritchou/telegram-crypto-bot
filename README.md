f3441d549cc4f69df14d19a08362028db2e265f7
gritchou <gritchou@gmail.com>
2021-02-14 13:40:01 +0100

    feat(bot): Added bot commands and documentation

--- README.md
+++ README.md
@@ -2,16 +2,36 @@
 
 A bot that can retrieve coins prices.
 
-## Setup
+usage: `/`${`COIN`}${`CURRENCY`}
 
-Set your variables:
+[link to the bot on telegram](http://t.me/CoingekoNoKyojinBot)
 
-- `BOT_TOKEN` (set is as an environment variable): the Token given by BotFather you when you create your bot on Telegram.
-- `API_KEY` (set is as an environment variable): Your Binance API key.
-- `API_SECRET` (set is as an environment variable): Your Binance API secret.
+## Envrionment variable
 
-## Vercel
+`BOT_TOKEN`: the Token given by BotFather you when you create your bot on Telegram.
+
+## Vercel devmode
 
 I use vercel to host my bot.
 Follow [this](https://www.marclittlemore.com/serverless-telegram-chatbot-vercel/) really cool tutorial from [@marclittlemore](https://twitter.com/marclittlemore).
 As he suggests, I used ngrok to test vercel's serveless function locally.
+
+Vercel will automatically use the `BOT_TOKEN` from your project on vercel.com
+
+You need to set NTBA_FIX_319 to fix an error with Promise cancellation [Github issue](https://github.com/yagop/node-telegram-bot-api/issues/319#issuecomment-324963294)
+
+To test in devmode:
+```sh
+NTBA_FIX_319=test vercel dev
+```
+
+## Bot commands
+
+- /help - lists bot commands
+- /usage - get examples
+- /btc - shorthand for BTCUSD
+- /eth - shorthand for ETHUSD
+- /coins - get the number of supported coins
+- /currencies - get the list of the supported currencies
+- /api - get the API used by this bot
+- /author me
--- api/webhook.js
+++ api/webhook.js
@@ -1,26 +1,75 @@
-const Binance = require('node-binance-api');
-const binance = new Binance().options({
-	API_KEY: process.env.API_KEY,
-	API_SECRET: process.env.API_SECRET
-});
 const TelegramBot = require('node-telegram-bot-api');
+const fetch = require('node-fetch');
 
-/*eslint-env node*/
-// https://github.com/yagop/node-telegram-bot-api/issues/319#issuecomment-324963294
-// Fixes an error with Promise cancellation
-// process.env.NTBA_FIX_319 = 'test';
+const CRYPTO_MAP = new Map();
+const CURRENCY_MAP = new Map();
+const SUPPORTED_CURRENCIES = [];
 
-const matchingWord = /(!btc|!eth)/ig;
+const COMMANDS = `/help - lists bot commands\n/usage - get examples\n/btc - shorthand for BTCUSD\n/eth - shorthand for ETHUSD\n/coins - get the number of supported coins\n/currencies - get the list of the supported currencies\n/api - get the API used by this bot\n/author - me`;
+const EXAMPLE = `Example usage: /\${COIN}\${CURRENCY} case insensitive\n/BTCUSD\n/etheur\n/dogeBTC\n/VETusd`;
 
 module.exports = async (request, response) => {
 	try {
+		/*eslint-env node*/
 		const bot = new TelegramBot(process.env.BOT_TOKEN);
 		const { message } = request.body;
 
-		let ticker = await binance.prices();
+		await fetch('https://api.coingecko.com/api/v3/coins/list')
+			.then((response) => response.json())
+			.then((coins) => coins.forEach((coin) => CRYPTO_MAP.set(coin.symbol, { id: coin.id, name: coin.name })))
+			;
 
-		if (message && message.text && matchingWord.test(message.text)) {
-			await bot.sendMessage(message.chat.id, `1 ${message.text.slice(1).toUpperCase()} = ${parseInt(ticker[message.text.slice(1).toUpperCase() + 'USDT']).toFixed(2)}$ = ${parseInt(ticker[message.text.slice(1).toUpperCase() + 'EUR']).toFixed(2)}€`);
+		await fetch('https://api.coingecko.com/api/v3/simple/supported_vs_currencies')
+			.then((response) => response.json())
+			.then((currencies) => currencies.forEach((currency) => SUPPORTED_CURRENCIES.push(currency)))
+			;
+
+		await fetch('https://www.localeplanet.com/api/auto/currencymap.json')
+			.then((response) => response.json())
+			.then((currencies) => SUPPORTED_CURRENCIES.forEach((currency) => CURRENCY_MAP.set(currency, currencies[currency.toUpperCase()] && currencies[currency.toUpperCase()].symbol || currency.toUpperCase())))
+			;
+
+		if (message && message.text) {
+			if (message.text === '/help') {
+				await bot.sendMessage(message.chat.id, COMMANDS);
+			} else if (message.text === '/usage') {
+				await bot.sendMessage(message.chat.id, EXAMPLE);
+			} else if (message.text === '/coins') {
+				await bot.sendMessage(message.chat.id, CRYPTO_MAP.size + ' coins supported.');
+			} else if (message.text === '/currencies') {
+				await bot.sendMessage(message.chat.id, 'Supported currencies: ' + SUPPORTED_CURRENCIES.join(', ') + ".");
+			} else if (message.text === '/btc') {
+				await fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
+					.then((response) => response.json())
+					.then((coin) => bot.sendMessage(message.chat.id, coin.market_data.current_price.usd + ' $'))
+					;
+			} else if (message.text === '/eth') {
+				await fetch('https://api.coingecko.com/api/v3/coins/ethereum')
+					.then((response) => response.json())
+					.then((coin) => bot.sendMessage(message.chat.id, coin.market_data.current_price.usd + ' $'))
+					;
+			} else if (message.text === '/api') {
+				await bot.sendMessage(message.chat.id, 'This bot uses CoinGecko API and localeplanet currencymap.');
+			} else if (message.text === '/author') {
+				await bot.sendMessage(message.chat.id, 'twitter.com/gritchou');
+			} else if (message.text.startsWith('/')) {
+				const query = message.text.slice(1).toLowerCase();
+				let currency = query.slice(-4);
+				if (!SUPPORTED_CURRENCIES.includes(currency)) {
+					currency = query.slice(-3);
+				}
+				if (!SUPPORTED_CURRENCIES.includes(currency)) {
+					await bot.sendMessage(message.chat.id, 'Invalid token pair: ' + message.text.slice(1));
+				}
+				let coin = query.slice(0, query.length - currency.length);
+				if (!CRYPTO_MAP.has(coin)) {
+					await bot.sendMessage(message.chat.id, 'Invalid token pair: ' + message.text.slice(1));
+				}
+				await fetch('https://api.coingecko.com/api/v3/coins/' + CRYPTO_MAP.get(coin).id)
+					.then((response) => response.json())
+					.then((coin) => bot.sendMessage(message.chat.id, coin.market_data.current_price[currency] + ' ' + CURRENCY_MAP.get(currency)))
+					;
+			}
 		}
 	}
 	catch (error) {
